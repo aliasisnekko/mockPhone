5c6244e75d74da06b8f449aa8f3eb3b2ba859328
Nekko Street <clayvon.street@gmail.com>
2021-04-01 22:34:44 -0700

    Delete JavaScript directory

--- JavaScript/main.js
+++
@@ -1,8 +0,0 @@
-const me = {
-        firstName: "Nekko",
-        lastName: "Street",
-        age: 20,
-        occupation: "Programmer"
-    }
-    // first time using template literals, know that backticks `` are used instead of quotation marks and strings can be concatenated without + symbol/written as plain english, know that expressions can be used by placing it between curly braces followed by $ ie ${expression}
-console.log(`Hello, I'm ${me.firstName} ${me.lastName}. I'm a ${me.age} ${me.occupation}`);
